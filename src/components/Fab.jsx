b62fcf6f40d7bd210f06e1b91dc147dbe5d1e8a0
nabil-skd <nabilshaikh7730@gmail.com>
2026-01-22 23:12:40 +0530

    chore: updated resume link

--- src/components/Fab.jsx
+++ src/components/Fab.jsx
@@ -1,8 +1,6 @@
 const Fab = () => {
   const googleDriveUrl =
     "https://drive.google.com/file/d/1U4o_ghXPgrXu9Hu7d0fyJumbNJmttesb/view?usp=sharing";
-  // "https://drive.google.com/file/d/1h5ET0UCymC-5GxMY7zz7h7vy8-zAEY0S/view?usp=sharing";
-
   return (
     <div className="fixed bottom-8 right-16">
       <button
